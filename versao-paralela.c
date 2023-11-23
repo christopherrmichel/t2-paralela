f8125c3fd978cd9ef0b01a3d4ad8f77a0d9b253f
Christopher Michel <christopher.michel@edu.pucrs.br>
2023-11-23 16:36:31 -0300

    -Teste

--- versao-paralela.c
+++ versao-paralela.c
@@ -10,11 +10,11 @@ int l1, c1, l2, c2, lres, cres;
 int main(int argc, char *argv[]) {
   int i, j, k, id, p;
   double elapsed_time;
-  MPI_Status status;
 
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
   MPI_Comm_rank(MPI_COMM_WORLD, &id);
+  MPI_Status status;
   /* Só faz sentido na versão sequencial
   if (id != 0) {
       MPI_Finalize();
@@ -34,11 +34,13 @@ int main(int argc, char *argv[]) {
 
   //Verificar se é 0 para saber se é o mestre
   if (id == 0) {
-    //Se for o mestre, precisa fazer o trabalho inicial de preencher as matrizes
+    // Popula as matrizes.
     int k = 1;
-    for (i=0; i<SIZE; i++) {
-      for (j=0 ; j<SIZE; j++) {
-        if (k%2==0)
+    for (int i = 0; i < SIZE; i++)
+    {
+      for (int j = 0; j < SIZE; j++)
+      {
+        if (k % 2 == 0)
           m1[i][j] = -k;
         else
           m1[i][j] = k;
@@ -46,81 +48,107 @@ int main(int argc, char *argv[]) {
       k++;
     }
     k = 1;
-    for (j=0 ; j<SIZE; j++) {
-        for (i=0 ; i<SIZE; i++) {
-	        if (k%2==0)
-               m2[i][j] = -k;
-	        else
-               m2[i][j] = k;
-        }
-        k++;
+    for (int j = 0; j < SIZE; j++)
+    {
+      for (int i = 0; i < SIZE; i++)
+      {
+        if (k % 2 == 0)
+          m2[i][j] = -k;
+        else
+          m2[i][j] = k;
+      }
+      k++;
     }
 
     // PREPARA PARA MEDIR TEMPO
+    // Comeca a contar depois que as matrizes estao populadas.
     elapsed_time = -MPI_Wtime();
 
-    //Todos os escravos precisam ter uma das matrizes completas, escolhemos mandar a M2
-    MPI_Bcast(m2, pow(SIZE,2), MPI_INT, 0, MPI_COMM_WORLD);
+    // BROADCAST PARA ENVIAR A MATRIZ 2 INTEIRA PARA TODOS OS ESCRAVOS
+    MPI_Bcast(m2, tamanhoMatriz, MPI_INT, 0, MPI_COMM_WORLD);
 
-    //Enviar parte da m1 para os escravos
-    for (i = 1; i < p; ++i) {
-      int idEscravo = i; 
+    // Loop para enviar partes da matriz 1 para os escravos.
+    for (int i = 0; i < p - 1; ++i)
+    {
+      int idEscravo = i + 1;
+      int idUltimoEscravo = p - 2;
 
-      int numeroDeLinhasPorProcesso = SIZE / (p - 1);
-      int linhaInicialDoProcesso = (i-1) * numeroDeLinhasPorProcesso;
-      if (i == (p - 1)) { //Verificar se é o último processo a ser enviado os dados
-        //Mandar somente o "resto", linhas restantes para o último
-        numeroDeLinhasPorProcesso += SIZE % (p - 1);
+      int qtdLinhas = SIZE / (p - 1);
+      int numLinha = i * qtdLinhas;
+      if (i == idUltimoEscravo)
+      {
+        // Se for o ultimo escravo, envia as linhas restantes.
+        qtdLinhas += SIZE % (p - 1);
       }
 
-      // Enviar a linha que o processo deve começar a calcular, a quantidade de linhas e as linhas da matriz
-      MPI_Send(&linhaInicialDoProcesso, 1, MPI_INT, idEscravo, 0, MPI_COMM_WORLD);
-      MPI_Send(&numeroDeLinhasPorProcesso, 1, MPI_INT, idEscravo, 0, MPI_COMM_WORLD);
-      MPI_Send(&m1[linhaInicialDoProcesso][0], numeroDeLinhasPorProcesso * SIZE, MPI_INT, idEscravo, 0, MPI_COMM_WORLD);
+      // SEND-NUMERO DA LINHA A COMECAR
+      MPI_Send(&numLinha, 1, MPI_INT, idEscravo, 0, MPI_COMM_WORLD);
+
+      // SEND-QUANTIDADE DE LINHAS A PROCESSAR
+      MPI_Send(&qtdLinhas, 1, MPI_INT, idEscravo, 0, MPI_COMM_WORLD);
+
+      // SEND-LINHAS M1
+      MPI_Send(&m1[numLinha][0], qtdLinhas * SIZE, MPI_INT, idEscravo, 0, MPI_COMM_WORLD);
     }
 
-    //For para pegar os retornos após o cálculos em cada escravo
-    for (int i = 1; i < p; ++i) {
-      int numeroDeLinhasPorProcesso;
-      int linhaInicialDoProcesso;
+    for (int i = 0; i < p - 1; ++i)
+    {
+      int numLinha, qtdLinhas;
 
-      //Usando o MPI_Recv para receber primeiro o numero da linha do intervalo que foi calculado
-      //Segundo a quantidade de linhas que vai ser fornecida no retorno e terceiro os valores da matriz resultante
-      MPI_Recv(&linhaInicialDoProcesso, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
-      MPI_Recv(&numeroDeLinhasPorProcesso, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
-      MPI_Recv(&mres[linhaInicialDoProcesso][0], numeroDeLinhasPorProcesso * SIZE, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &status);
+      // RECEIVE-NUMERO DA LINHA A COMECAR
+      MPI_Recv(&numLinha, 1, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
+      int source = status.MPI_SOURCE;
+
+      // RECEIVE-QUANTIDADE DE LINHAS A PROCESSAR
+      MPI_Recv(&qtdLinhas, 1, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
+      
+      // RECEIVE-LINHAS M RESULTANTE
+      MPI_Recv(&mres[numLinha][0], qtdLinhas * SIZE, MPI_INT, source, 0, MPI_COMM_WORLD, &status);
     }
 
     // OBTEM O TEMPO
+    // Para de contar depois que a matriz resultante esta pronta.
     elapsed_time += MPI_Wtime();
     printf("%d - %lf\n", SIZE, elapsed_time);
 
     // VERIFICA SE O RESULTADO DA MULTIPLICACAO ESTA CORRETO
-    for (i=0 ; i<SIZE; i++) {
-      k = SIZE*(i+1);
-      for (j=0 ; j<SIZE; j++) {
-          int k_col = k*(j+1);
-          if (i % 2 ==0) {
-             if (j % 2 == 0) {
-                if (mres[i][j]!=k_col)
-                   return 1;
-             }
-             else {
-                if (mres[i][j]!=-k_col)
-                   return 1;
-             }
+    for (int i = 0; i < SIZE; i++)
+    {
+      int k = SIZE * (i + 1);
+      for (int j = 0; j < SIZE; j++)
+      {
+        int k_col = k * (j + 1);
+        if (i % 2 == 0)
+        {
+          if (j % 2 == 0)
+          {
+            if (mres[i][j] != k_col)
+              printf("\nERRO validando matriz resultante: %d != %d", mres[i][j], k_col);
+              return 1;
           }
-          else {
-             if (j % 2 == 0) {
-                if (mres[i][j]!=-k_col)
-                   return 1;
-             }
-             else {
-                if (mres[i][j]!=k_col)
-                   return 1;
-             }
+          else
+          {
+            if (mres[i][j] != -k_col)
+              printf("\nERRO validando matriz resultante: %d != %d", mres[i][j], -k_col);
+              return 1;
           }
-      } 
+        }
+        else
+        {
+          if (j % 2 == 0)
+          {
+            if (mres[i][j] != -k_col)
+              printf("\nERRO validando matriz resultante: %d != %d", mres[i][j], -k_col);
+              return 1;
+          }
+          else
+          {
+            if (mres[i][j] != k_col)
+              printf("\nERRO validando matriz resultante: %d != %d", mres[i][j], k_col);
+              return 1;
+          }
+        }
+      }
     }
   }
   // Se for diferente de 0, significa que o processo é um escravo
